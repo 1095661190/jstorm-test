558233cf35eb53e405cea30d028457e62ac0ad86
1095661190@qq.com <lijun.wen@happyelements.com>
2021-04-27 15:27:31 +0800

    jstorm

--- src/main/java/org/example/RemoteTopology.java
+++ src/main/java/org/example/RemoteTopology.java
@@ -49,6 +49,7 @@ public class RemoteTopology {
         StormSubmitter.submitTopology("streamName", conf,builder.createTopology());
         //提交topology
 
+        System.out.println("..............");
         System.out.println("test..............");
 
     }
