738b92ad6867362d664fae76fd4bf05f5349ee0b
1095661190@qq.com <lijun.wen@happyelements.com>
2021-09-02 18:43:35 +0800

    tools

---
+++ src/main/java/org/example/Tools.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Tools {
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public static boolean isEmpty(CharSequence cs) {
+        return cs == null || cs.length() == 0;
+    }
+
+}
